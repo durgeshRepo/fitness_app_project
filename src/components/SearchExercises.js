a988f55ab80899a482a5a0cdc84cc78743571640
Durgesh <djthapaniya1951@gmail.com>
2023-09-16 23:44:37 +0530

    Added and Styled Search Bar Component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="50px" textAlign="center">
+        Awesome Exercises You <br />
+        Should Know
+      </Typography>
+
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '800px', xs: '350px' },
+            backgroundColor: '#fff', borderRadius: '50px'
+          }}
+          height="76px" value="" onChange={(e) => { }} placeholder="Search Exercises" type="text"
+        />
+
+        <Button className="search-btn"
+          sx={{
+            bgcolor: '#ff2625', color: '#fff', textTransform: 'none', width: { lg: '175px', xs: '80px' },
+            fontSize: { lg: '20px', xs: '14px' }, height: '56px', position: 'absolute', right: '0'
+          }}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
